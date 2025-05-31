5f4e60868295dfb26f8c7ead8dbe6bf955264a36
L00166873 <L00166873@atu.ie>
2025-05-31 16:12:03 +0000

    Work already

--- model_test.py
+++ model_test.py
@@ -4,10 +4,10 @@ from model import get_data, tt_split
 #basically, assert takes in boolean logic. so, to test if something works,
 #you'd put something to get a 'True' or a 'False'. the rest is self explanatory. 
 def test_get_data():
-    df = get_data("../dataset.csv")
+    df = get_data("dataset.csv")
     assert df[['Hours']].shape == df['Scores'].shape
 
 def test_tt_split():
-    x_tr, x_te, y_tr, y_te = tt_split(get_data("../dataset.csv"))
+    x_tr, x_te, y_tr, y_te = tt_split(get_data("dataset.csv"))
     assert x_tr.shape == y_tr.shape
     assert x_te.shape == y_te.shape
